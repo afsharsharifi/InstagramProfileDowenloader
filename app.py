fd6c91e60eb6f544aa0edb3b9166273f1892964d
Afshar Sharifi <afsharsharifi2020@gmail.com>
2022-01-02 13:25:49 +0330

    Folder Create Fix

--- app.py
+++ app.py
@@ -1,7 +1,17 @@
-from flask import Flask, request, render_template, send_file, redirect
+import os
+from flask import Flask, request, render_template, redirect
+from flask.scaffold import F
 from instaloader import Instaloader
-from os import listdir, rmdir, path
+from os import listdir, path
 from shutil import move, rmtree
+
+
+def CreateDir():
+    if os.path.exists("static/Profiles/"):
+        print("Folder Exists")
+    else:
+        os.makedirs("static/Profiles/")
+        print("Folder Created")
 
 
 insta_username = Instaloader()
@@ -12,6 +22,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
+    CreateDir()
     return render_template("form.html")
 
 
@@ -19,9 +30,9 @@ def index():
 def showResult():
     username = request.args.get("username", "No Username")
     insta_username.download_profile(username, profile_pic_only=True)
-    pathList = listdir(username)
-    picName = pathList[0]
-    fullPathSRC = file_dir + "\\" + username + "\\" + pathList[0]
+    picName = listdir(username)[0]
+
+    fullPathSRC = file_dir + "\\" + username + "\\" + picName
     fullPathDEC = file_dir + "\\" + "static\Profiles"
     try:
         move(fullPathSRC, fullPathDEC)
